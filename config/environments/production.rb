c5111723dc9d984e6abe39b27139636074ebdc71
kishorecherukuror <kishorecheruku.ror@gmail.com>
2017-07-14 11:44:04 +0530

    Mailing seting in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,18 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "shopping_#{Rails.env}"
-  config.action_mailer.perform_caching = false
+  config.action_mailer.perform_caching = true
+  config.action_mailer.delivery_method = :smtp
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+   :address              => "smtp.gmail.com",
+   :port                 => 587,
+   :user_name            => "kishorebatch38@gmail.com",
+   :password             => "Batch38@123",
+   :authentication       => "plain",
+  :enable_starttls_auto => true
+  }
+
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
